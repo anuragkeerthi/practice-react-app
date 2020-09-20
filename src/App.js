c50372f2e9238ed5d40b7eaa54785501d21df7cf
Anurag Keerthi <anuragkeerthi@gmail.com>
2020-09-20 17:04:45 +0530

    Using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+
+
+  const app = props => {
+   const [personsState, setPersonsState] = useState({
+      persons : [
+        {name: 'Anurag', age: 23},
+        {name: 'Bond', age: 29}
+      ],
+      otherState : "someother Value"
+    });
+
+    const switchNameHandler = () => {
+      // this.state.persons[0].name = "Anurag Keerthi";
+      setPersonsState({persons : [
+        {name: 'Anurag Keerthi', age: 23},
+        {name: 'Bond', age: 50}
+      ],
+      otherState : personsState.otherState
+    });
+    };
+
     return (
       <div className="App">
          <h1>Testing...</h1>
-         <Person name="Anurag" age="23" />
-         <Person name="Bond" age="50">Hobbies : Shooting</Person>
+         <button onClick={switchNameHandler}>Switch name</button>
+         <Person name= {personsState.persons[0].name} age={personsState.persons[0].age} />
+         <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Hobbies : Shooting</Person>
       </div>
     );
     //return React.createElement('div',{className : 'App'}, React.createElement('h1',null,'Hi, This is H1 Tag'));
 
   }
-}
 
-export default App;
+
+export default app;
+
+  
